2a0f9d5d72067cc2d10cc344b0d82dcbbffe4840
laisarlindo <159180549+laisarlindo@users.noreply.github.com>
2024-08-13 13:35:26 -0300

    Add files via upload

---
+++ ATIVIDADE02/prjBanco/ContaBancaria.java
@@ -0,0 +1,21 @@
+package prjBanco;
+
+public class ContaBancaria {
+	//ATRIBUTOS
+	private int numeroConta;
+	private String nomeTitular;
+	private double Saldo;
+	
+	// CONSTRUTORES
+	public ContaBancaria(int numeroConta,String nomeTitular, double Saldo){
+		
+		 this.numeroConta= numeroConta;
+		 this.nomeTitular = nomeTitular;
+		 this.Saldo = Saldo;
+	}
+	public ContaBancaria() {
+	}
+	
+	//Getters Setters
+	
+}
---
+++ ATIVIDADE02/prjBanco/ContaBanco.java
@@ -0,0 +1,10 @@
+package prjBanco;
+
+public class ContaBanco {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
