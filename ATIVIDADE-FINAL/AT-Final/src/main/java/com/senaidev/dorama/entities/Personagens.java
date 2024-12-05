2275aef4c018eaeab241c15e00edeb6c4a0b8a58
laisarlindo <159180549+laisarlindo@users.noreply.github.com>
2024-12-05 08:58:17 -0300

    Add files via upload

--- ATIVIDADE-FINAL/AT-Final/src/main/java/com/senaidev/dorama/entities/Personagens.java
+++ ATIVIDADE-FINAL/AT-Final/src/main/java/com/senaidev/dorama/entities/Personagens.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +25,10 @@ public class Personagens {
 		
 		@Column(name = "pontoForte",nullable = false)
 		private String pontoForte;
+		
+		@ManyToOne
+		@JoinColumn(name = "DORAMA_id_dorama")
+		private Dorama dorama;
 		
 		//CONTRUTORES
 		public Personagens() {
